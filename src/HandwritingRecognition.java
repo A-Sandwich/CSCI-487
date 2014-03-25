425e54b9bb46edf4ea11c7e15f848385dda56b00
thedouglenz <thedouglenz@gmail.com>
2014-03-25 18:30:56 -0400

    stuff

---
+++ src/HandwritingRecognition.java
@@ -0,0 +1,10 @@
+import Jama.Matrix;
+
+/**
+ * Created by Doug on 3/25/2014.
+ */
+public class HandwritingRecognition {
+    public static void main(String[] args) {
+        Matrix m = new Matrix(5,5,10);
+    }
+}
--- src/main/java.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-/**
- * Created by kb on 3/25/14.
- */
-public class java {
-}
