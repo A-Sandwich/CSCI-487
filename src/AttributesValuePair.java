ffd279584e4ebd576aa321ff06ae2ef208566a32
Douglas Lenz <thedouglenz@gmail.com>
2014-03-27 00:44:02 -0400

    Made a class for a sample...
    called AtributesValuePair to contain one pattern that represents a
    handwriting sample and one character to represent the value that should
    be associated with the handwriting sample

---
+++ src/AttributesValuePair.java
@@ -0,0 +1,12 @@
+/**
+ * Created by douglas lenz on 3/27/2014.
+ */
+public class AttributesValuePair {
+    private double[][] charPattern; // Attributes
+    private char character; // Value
+
+    public AttributesValuePair(double[][] pattern, char value) {
+        this.charPattern = pattern;
+        this.character = value;
+    }
+}
