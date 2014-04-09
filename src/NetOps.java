4cc44e898b3c40f182d8c45f0e7652f55caefaf0
A-Sandwich <kbburkholder@gmail.com>
2014-04-09 09:59:22 -0400

    Seems to be working.

--- src/NetOps.java
+++ src/NetOps.java
@@ -21,17 +21,19 @@ public class NetOps {
     public NetOps(){
         //CONSTRUCTOR
 
-        trainingSet = new DataSet(35,1);
+        trainingSet = new DataSet(35,26);
 
-        neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.SIGMOID, 35, 100, 1);
+        neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.SIGMOID, 35, 100, 26);
         //neurons = new Neuron[rawData.length*rawData[0].length];
     }
 
     public DataSet createDataset(int[][] rawData, char target){
         double[] data1D;
 
+        double[] target_array = getTargetArray(target);
+
         data1D = convertArrays(rawData);
-        trainingSet.addRow(new DataSetRow(data1D, new double[]{((double)target)/100}));
+        trainingSet.addRow(new DataSetRow(data1D, target_array));
         return trainingSet;
     }//end createDataSet
 
@@ -41,9 +43,10 @@ public class NetOps {
 
     public void test(int[][] rawData, char target){
         double[] data1d;
+        double[] target_array = getTargetArray(target);
         data1d = convertArrays(rawData);
-        DataSet test = new DataSet(35, 1);
-        test.addRow(new DataSetRow(data1d, new double[]{(double)target}));
+        DataSet test = new DataSet(35, 26);
+        test.addRow(new DataSetRow(data1d, target_array));
         testNeuralNetwork(neuralNetwork, test);
     }
 
@@ -71,16 +74,43 @@ public class NetOps {
     }
 
     public static void testNeuralNetwork(NeuralNetwork nnet, DataSet tset) {
+            int max = 0, second_best = 0;
 
             for(DataSetRow dataRow : tset.getRows()) {
 
             nnet.setInput(dataRow.getInput());
             nnet.calculate();
             double[ ] networkOutput = nnet.getOutput();
-            System.out.print("Input: " + Arrays.toString(dataRow.getInput()) );
+
+            for(int i = 0; i < networkOutput.length; i++){
+                if(networkOutput[i] > networkOutput[max]){
+                    if(networkOutput[max] > networkOutput[second_best])
+                        second_best = max;
+                    if(networkOutput[i] > networkOutput[max])
+                        max = i;
+                }//end if
+
+            }//end for
+
+            System.out.println("Input: " + Arrays.toString(dataRow.getInput()) );
             System.out.println(" Output: " + Arrays.toString(networkOutput) );
+            System.out.println("Guess 1: "+(char)(max+65)+" (Probability: "+(int)(networkOutput[max] * 100)+"% )");
+            System.out.println("Guess 2: "+(char)(second_best+65)+" (Probability: "+(int)(networkOutput[second_best] * 100)+"% )");
 
         }//end for
 
     }//end testNeuralNetwork
+
+    public double[] getTargetArray(char target){
+        double[] target_array = new double[26];
+
+        for(int i = 0; i < target_array.length; i++){
+            if(i == ((int)target)-65)
+                target_array[i] = 1;
+            else
+                target_array[i] = 0;
+        }//end for
+
+        return target_array;
+    }
 }
